f9c39828755e70d2b6e35f1f36d7cae21392d1f1
ichigo-dev <ichigo@web-freework.com>
2021-01-27 17:20:59 +0900

    update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,5 +1,16 @@
 class User
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
   def hello
-    puts 'hello'
+    puts "hello,#{@name}"
   end
 end
+
+user = User.new('hoge')
+user.hello
+
+user.name = 'fuga'
+user.hello
