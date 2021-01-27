61f422adef77698595f35eb93a1ac1cb19b2391a
ichigo-dev <ichigo@web-freework.com>
2021-01-27 23:26:47 +0900

    update

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,20 +3,21 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < MiniTest::Test
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+    @ticket = Ticket.new(150)
+  end
+
   def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
-    ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(@ticket)
+    assert @juso.exit(@ticket)
   end
 
   def test_umeda_to_juso
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
-    ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    
+    @umeda.enter(@ticket)
+    refute @mikuni.exit(@ticket)
   end
 end
